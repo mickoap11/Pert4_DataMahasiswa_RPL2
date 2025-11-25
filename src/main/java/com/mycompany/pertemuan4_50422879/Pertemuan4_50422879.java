2a56ad020ca3f4abab641c8ccccb5934466d10f9
mickoap11 <mickoaryapra@gmail.com>
2025-11-25 16:23:32 +0700

    First Commit

---
+++ src/main/java/com/mycompany/pertemuan4_50422879/Pertemuan4_50422879.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pertemuan4_50422879;
+
+/**
+ *
+ * @author User
+ */
+public class Pertemuan4_50422879 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/com/mycompany/pertemuan4_50422879/controller/MahasiswaController.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.pertemuan4_50422879.controller;
+
+import com.mycompany.pertemuan4_50422879.model.ModelMahasiswa;
+import java.util.List;
+
+/**
+ *
+ * @author User
+ */
+public interface MahasiswaController {
+    public void addMhs(ModelMahasiswa mhs);
+    public ModelMahasiswa getMhs(int id);
+    public void updateMhs(ModelMahasiswa mhs);
+    public void deleteMhs(int id);
+    public List<ModelMahasiswa> getAllMahasiswa();
+}
+
